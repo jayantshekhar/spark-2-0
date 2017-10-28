71029065ebd4760b0c56a530eeec7eb5ee8a848e
jayantshekhar <jayant@Jayants-MacBook-Pro.local>
2017-10-27 23:51:07 -0700

    Spark

--- README.md
+++ README.md
@@ -1,26 +1,24 @@
-# Strata-2016-NYC
+# Spark
 
 ## Pre-requisites for Installation
 
 Java installed on the laptop
 
-## Download Spark 2.0
+## Download Spark 2.2.0
 
-Download Spark 2.0 from here : http://spark.apache.org/downloads.html
+Download Spark 2.2.0 from here : http://spark.apache.org/downloads.html
 
-Direct Download link : http://d3kbcqa49mib13.cloudfront.net/spark-2.0.0-bin-hadoop2.7.tgz
+## Install Spark 2.2.0 on Mac
 
-## Install Spark 2.0 on Mac
+tar -zxvf spark-2.2.0-bin-hadoop2.7.tgz
 
-tar -zxvf spark-2.0.0-bin-hadoop2.7.tgz
+export PATH=$PATH:/Users/path_to_downloaded_spark/spark-2.2.0-bin-hadoop2.7/bin
 
-export PATH=$PATH:/Users/path_to_downloaded_spark/spark-2.0.0-bin-hadoop2.7/bin
+## Install Spark 2.2.0 on Windows
 
-## Install Spark 2.0 on Windows
+Add the spark bin directory to Path : ...\spark-2.2.0-bin-hadoop2.7\bin
 
-Add the spark bin directory to Path : ...\spark-2.0.0-bin-hadoop2.7\bin
-
-tar -zxvf spark-2.0.0-bin-hadoop2.7.tgz
+tar -zxvf spark-2.2.0-bin-hadoop2.7.tgz
 
 ## Git
 
@@ -29,8 +27,8 @@ Nice to have
 [IMPORTANT]: Downloads
 Have the following downloaded before the session
 * Spark binaries
-* JDK installed (> 1.7.x)
-* https://github.com/WhiteFangBuck/strata-2016-ny
+* JDK installed
+
 
 
 
---
+++ src/main/java/org/workshop/SparkBasics.java
@@ -0,0 +1,69 @@
+package org.workshop;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.rdd.RDD;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by jayant on 10/27/17.
+ */
+public final class SparkBasics {
+
+    public static void main(String[] args) throws Exception {
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("SparkBasics").master("local")
+                .getOrCreate();
+
+        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+
+        parallelizedCollections(jsc);
+
+        readFile(jsc);
+
+        readTransformAct(jsc);
+
+        passingFunctions(jsc);
+
+        spark.stop();
+    }
+
+    public static void parallelizedCollections(JavaSparkContext jsc) {
+        List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
+        JavaRDD<Integer> distData = jsc.parallelize(data);
+
+        distData.collect().forEach(i -> System.out.println(i));
+    }
+
+    public static void readFile(JavaSparkContext jsc) {
+        JavaRDD<String> distFile = jsc.textFile("README", 1);
+    }
+
+    public static void readTransformAct(JavaSparkContext jsc) {
+        JavaRDD<String> lines = jsc.textFile("data.txt", 1);
+        JavaRDD<Integer> lineLengths = lines.map(s -> s.length());
+
+        int totalLength = lineLengths.reduce((a, b) -> a + b);
+    }
+
+
+    public static void passingFunctions(JavaSparkContext jsc) {
+        JavaRDD<String> lines = jsc.textFile("data.txt", 1);
+
+        JavaRDD<Integer> lineLengths = lines.map(new Function<String, Integer>() {
+            public Integer call(String s) { return s.length(); }
+        });
+
+        int totalLength = lineLengths.reduce(new Function2<Integer, Integer, Integer>() {
+            public Integer call(Integer a, Integer b) { return a + b; }
+        });
+
+    }
+}
