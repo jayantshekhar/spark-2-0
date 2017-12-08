377f5668b5401368e36f09c51b43d036b0dff143
jayantshekhar <jayant@Jayants-MacBook-Pro.local>
2017-12-08 10:50:34 -0800

    flights...

--- src/main/java/org/workshop/Flights.java
+++ src/main/java/org/workshop/Flights.java
@@ -105,42 +105,7 @@ public class Flights {
 
         results = joinDataframe.select("ORIGIN");
         results.show();
-
-
-        // Convert records of the RDD (people) to Rows
-	/* JavaRDD<Row> rowRDD = flightRdd.map(new Function<String, Row>() {
-	   public Row call(String record) throws Exception {
-	     String[] attributes = record.split(",");
-	     return RowFactory.create(attributes[0], attributes[1], attributes[2], attributes[3], attributes[4]
-	    		 , attributes[5], attributes[6], attributes[7], attributes[8], attributes[9], attributes[10]
-	    				 , attributes[11], attributes[12], attributes[13], attributes[14], attributes[15], attributes[16].trim());
-	   }
-	 });
-	*/
-
-        // The schema is encoded in a string
-	/* String schemaString = "field1 field2 field3 field4 field5 field6 field7 field8 field9 field10 field11 field12 field13 field14 field15 field16 field17";
-
-	 // Generate the schema based on the string of schema
-	 List<StructField> fields = new ArrayList<>();
-
-	 for (String fieldName : schemaString.split(" ")) {
-	   StructField field = DataTypes.createStructField(fieldName, DataTypes.StringType, true);
-	   fields.add(field);
-	 }
-	 StructType schema = DataTypes.createStructType(fields);
-
-	// Apply the schema to the RDD.
-			 Dataset<Row> flightsDataFrame = sqlContext.createDataFrame(rowRDD, schema);
-
-			 // Register the DataFrame as a table.
-			 flightsDataFrame.registerTempTable("flight_details");
-
-			 // SQL can be run over RDDs that have been registered as tables.
-			 Dataset<Row> results = sqlContext.sql("SELECT * FROM flight_details");
-			 results.show();*/
-
-
+        
     }
 
     public static JavaRDD<Flight> createRDD(SparkSession sparkSession, String inputFile) {
