ab8008bbbd949033b0e9161fe8ea322ceaa08c4d
jayantshekhar <jayant@Jayants-MacBook-Pro.local>
2017-12-08 10:28:47 -0800

    flights

--- src/main/java/org/workshop/Flights.java
+++ src/main/java/org/workshop/Flights.java
@@ -3,23 +3,71 @@ package org.workshop;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SQLContext;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Flights {
 
-    public static void main(String[] args) throws  org.apache.spark.sql.AnalysisException{
+    public static void main(String[] args) throws  org.apache.spark.sql.AnalysisException {
 
-        final SparkConf sparkConf = new SparkConf().setAppName("FlightData").setMaster("local");
-        JavaSparkContext sc = new JavaSparkContext(sparkConf) ;
+        //final SparkConf sparkConf = new SparkConf().setAppName("FlightData").setMaster("local");
+        //JavaSparkContext sc = new JavaSparkContext(sparkConf) ;
+
         //RDD of flight data
-        JavaRDD<String> flightRdd = sc.textFile("D:/Projects/SPARK_2_0/spark-2-0/data/flights_data.csv");
-        SQLContext sqlContext = new org.apache.spark.sql.SQLContext(sc);
+        //JavaRDD<String> flightRdd = sc.textFile("data/flights_data.csv");
+        //SQLContext sqlContext = new org.apache.spark.sql.SQLContext(sc);
 
         SparkSession spark = SparkSession.builder().master("local").appName("FlightData").config("spark.some.config.option", "some-value")
                 .getOrCreate();
+
+        rdd(spark);
+        //dataframe(spark);
+    }
+
+    public static void rdd(SparkSession spark) {
+        // $example on:programmatic_schema$
+        // Create an RDD
+        JavaRDD<String> peopleRDD = spark.sparkContext()
+                .textFile("data/flights_data.csv", 1)
+                .toJavaRDD();
+
+        // The schema is encoded in a string
+        //String schemaString = "DAY_OF_MONTH DAY_OF_WEEK CARRIER TAIL_NUM FL_NUM ORIGIN_AIRPORT_ID ORIGIN DEST_AIRPORT_ID DEST CRS_DEP_TIME DEP_TIME DEP_DELAY_NEW CRS_ARR_TIME ARR_TIME ARR_DELAY_NEW CRS_ELAPSED_TIME DISTANCE";
+
+        String schemaString = "DAY_OF_MONTH DAY_OF_WEEK";
+
+        // Generate the schema based on the string of schema
+        List<StructField> fields = new ArrayList<>();
+        for (String fieldName : schemaString.split(" ")) {
+            StructField field = DataTypes.createStructField(fieldName, DataTypes.StringType, true);
+            fields.add(field);
+        }
+        StructType schema = DataTypes.createStructType(fields);
+
+        // Convert records of the RDD (people) to Rows
+        JavaRDD<Row> rowRDD = peopleRDD.map(new Function<String, Row>() {
+            @Override
+            public Row call(String record) throws Exception {
+                String[] attributes = record.split(",");
+                return RowFactory.create(attributes[0], attributes[1].trim());
+            }
+        });
+
+        // Apply the schema to the RDD
+        Dataset<Row> df = spark.createDataFrame(rowRDD, schema);
+
+        df.show();
+
+    }
+
+    public static void dataframe(SparkSession spark) {
+
         //create dataframe from csv file(flight_data)
         Dataset<Row> flightDataframe = spark.read().option("header","true").csv("data/flights_data.csv");
 
@@ -33,6 +81,15 @@ public class Flights {
         joinDataframe.show();
 
 
+        // Creates a temporary view using the DataFrame
+        joinDataframe.createOrReplaceTempView("flights");
+
+        // SQL can be run over a temporary view created using DataFrames
+        Dataset<Row> results = spark.sql("SELECT ORIGIN FROM flights");
+        results.show();
+
+        results = joinDataframe.select("ORIGIN");
+        results.show();
 
 
         // Convert records of the RDD (people) to Rows
